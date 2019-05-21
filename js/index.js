7efe490c3fda71af2f0acb47d2359dafbd207865
gky360 <gky360@gmail.com>
2019-05-22 06:38:53 +0900

    render game of life in html canvas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 import("../crate/pkg").then(module => {
-  module.run();
+  const { Universe } = module;
+  const pre = document.getElementById("game-of-life-canvas");
+  const universe = Universe.new();
+
+  const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+  };
+
+  requestAnimationFrame(renderLoop);
 });
